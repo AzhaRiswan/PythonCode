5e67fd8f761d198430e60457d34a829232ba4784
unknown <azgaf86@gmail.com>
2020-12-06 22:18:05 +0530

    First Commit

---
+++ MyFirstPython.py
@@ -0,0 +1,14 @@
+Print("Hello I'm Azha")
+
+Print("This is my first python code in github")
+
+for i in range(4):
+	for j in range(4-i):
+		print("* ", end="")
+	print()
+	
+
+for i in range(4):
+	for j in range(4-i):
+		print(j+i+1, end="")
+	print()
